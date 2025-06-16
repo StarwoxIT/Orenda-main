e00c86a5cc07a149dc3670eaeed8078ad3277fa0
vehktaur <victorakhihiero@gmail.com>
2025-06-16 14:47:37 +0100

    Refactor teamData and CSS styles for consistency and readability
    - Updated teamData.js to use double quotes for strings and removed unnecessary trailing commas.
    - Reorganized team member entries for clarity and consistency.
    - Modified index.css to use double quotes for URLs and font-family declarations.
    - Cleaned up CSS rules by removing unnecessary whitespace and ensuring consistent formatting.
    - Added a new utility class `.box_center` for centering elements.

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -1,202 +1,197 @@
-import teamImages from '../assets/team_members/teamImages';
+import teamImages from "../assets/team_members/teamImages";
 
 const teamData = [
   {
     image: teamImages.gretchel,
-    name: 'Gretchel',
-    position: 'Admin Manager'
+    name: "Gretchel",
+    position: "Admin Manager",
   },
   {
     image: teamImages.memsy,
-    name: 'Memsy',
-    position: ' Overseas Admin Manager'
+    name: "Memsy",
+    position: " Overseas Admin Manager",
   },
   {
-    image: teamImages.keishla,
-    name: 'Keishla',
-    position: 'RN, Director of Clinical Support'
+    image: teamImages.carlos,
+    name: "Carlos",
+    position: "RN, Director of Clinical Support",
   },
   {
     image: teamImages.mmeno,
-    name: 'Mmeno',
-    position: 'Intake Manager'
+    name: "Mmeno",
+    position: "Intake Manager",
   },
   {
     image: teamImages.cess,
-    name: 'Cess',
-    position: 'Billing Manager'
+    name: "Cess",
+    position: "Billing Manager",
   },
   {
     image: teamImages.righteous,
-    name: 'Righteous',
-    position: 'Credentialing Manager'
+    name: "Righteous",
+    position: "Credentialing Manager",
   },
   {
     image: teamImages.progress,
-    name: 'Progress',
-    position: 'Tech Solutions Manager'
+    name: "Progress",
+    position: "Tech Solutions Manager",
   },
   {
     image: teamImages.darianny,
-    name: 'Darianny',
-    position: 'Client Support Specialist'
+    name: "Darianny",
+    position: "Client Support Specialist",
   },
   {
     image: teamImages.sylvia,
-    name: 'Sylvia',
-    position: 'Intake Specialist'
+    name: "Sylvia",
+    position: "Intake Specialist",
   },
   {
     image: teamImages.shaina,
-    name: 'Shaina',
-    position: 'Intake Specialist'
+    name: "Shaina",
+    position: "Intake Specialist",
   },
   {
     image: teamImages.nora,
-    name: 'Nora',
-    position: 'Intake Specialist'
+    name: "Nora",
+    position: "Intake Specialist",
   },
   {
     image: teamImages.precious,
-    name: 'Precious',
-    position: 'Intake Specialist'
+    name: "Precious",
+    position: "Intake Specialist",
   },
   {
     image: teamImages.cj,
-    name: 'CJ',
-    position: 'Client Support Specialist'
+    name: "CJ",
+    position: "Client Support Specialist",
   },
   {
     image: teamImages.valerie,
-    name: 'Valerie',
-    position: 'Client Support Specialist'
+    name: "Valerie",
+    position: "Client Support Specialist",
   },
   {
     image: teamImages.rhei,
-    name: 'Rhei',
-    position: 'Client Support Specialist'
+    name: "Rhei",
+    position: "Client Support Specialist",
   },
   {
     image: teamImages.ime,
-    name: 'Ime',
-    position: 'Intake Specialist'
+    name: "Ime",
+    position: "Intake Specialist",
   },
   {
     image: teamImages.oly,
-    name: 'Oly',
-    position: 'Intake Specialist'
+    name: "Oly",
+    position: "Intake Specialist",
   },
   {
     image: teamImages.liza,
-    name: 'Liza',
-    position: 'Clinical Support Specialist'
+    name: "Liza",
+    position: "Clinical Support Specialist",
   },
   {
     image: teamImages.kevin,
-    name: 'Kevin',
-    position: 'Intake Specialist'
+    name: "Kevin",
+    position: "Intake Specialist",
   },
 
   {
     image: teamImages.justine,
-    name: 'Justine',
-    position: 'Intake Specialist'
+    name: "Justine",
+    position: "Intake Specialist",
   },
   {
     image: teamImages.ella,
-    name: 'Ella',
-    position: 'Biller'
-  },
-  {
-    image: teamImages.carlos,
-    name: 'Carlos',
-    position: 'Clinical Support Specialist'
+    name: "Ella",
+    position: "Biller",
   },
   {
     image: teamImages.angel,
-    name: 'Angel',
-    position: 'Client Support Specialist'
+    name: "Angel",
+    position: "Client Support Specialist",
   },
   {
     image: teamImages.patty,
-    name: 'Patty',
-    position: 'Client Support Specialist'
+    name: "Patty",
+    position: "Client Support Specialist",
   },
   {
     image: teamImages.nikko,
-    name: 'Nikko',
-    position: 'Credentialing Specialist'
+    name: "Nikko",
+    position: "Credentialing Specialist",
   },
   {
     image: teamImages.fretzie,
-    name: 'Fretzie',
-    position: 'Credentialing Specialist'
+    name: "Fretzie",
+    position: "Credentialing Specialist",
   },
   {
     image: teamImages.jirah,
-    name: 'Jirah',
-    position: 'Credentialing Specialist'
+    name: "Jirah",
+    position: "Credentialing Specialist",
   },
   {
     image: teamImages.neil,
-    name: 'Neil',
-    position: 'Client Specialist'
+    name: "Neil",
+    position: "Client Specialist",
   },
   {
     image: teamImages.john_patrick,
-    name: 'John Patrick',
-    position: 'Clinical Support Specialist'
+    name: "John Patrick",
+    position: "Clinical Support Specialist",
   },
   {
     image: teamImages.jo,
-    name: 'Jo',
-    position: 'Biller'
+    name: "Jo",
+    position: "Biller",
   },
   {
     image: teamImages.jerricoh,
-    name: 'Jerricoh',
-    position: 'Client Support Specialist'
+    name: "Jerricoh",
+    position: "Client Support Specialist",
   },
   {
     image: teamImages.jerimine,
-    name: 'Jerimine',
-    position: 'Client Support Specialist'
+    name: "Jerimine",
+    position: "Client Support Specialist",
   },
   {
     image: teamImages.jeremie,
-    name: 'Jeremie',
-    position: 'Credentialing Specialist'
+    name: "Jeremie",
+    position: "Credentialing Specialist",
   },
   {
     image: teamImages.jaylo,
-    name: 'Jaylo',
-    position: 'Intake Specialist'
+    name: "Jaylo",
+    position: "Intake Specialist",
   },
   {
     image: teamImages.itoro,
-    name: 'Itoro',
-    position: 'Intake Specialist'
+    name: "Itoro",
+    position: "Intake Specialist",
   },
   {
     image: teamImages.phoebe,
-    name: 'Phoebe',
-    position: 'Intake Specialist'
+    name: "Phoebe",
+    position: "Intake Specialist",
   },
   {
     image: teamImages.gene,
-    name: 'Gene',
-    position: 'Intake Specialist'
+    name: "Gene",
+    position: "Intake Specialist",
   },
   {
     image: teamImages.emmeline,
-    name: 'Emmeline',
-    position: 'Intake Specialist'
+    name: "Emmeline",
+    position: "Intake Specialist",
   },
   {
     image: teamImages.jessabeth,
-    name: 'Jessabeth',
-    position: 'Client Support Specialist'
-  }
+    name: "Jessabeth",
+    position: "Client Support Specialist",
+  },
 ];
 
 export default teamData;
